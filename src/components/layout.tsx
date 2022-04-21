d462351d9499f27e5ad08598f7a9b0816170d9bd
Shalva Gegia <shakogegia@users.noreply.github.com>
2022-04-21 18:54:11 +0000

    fix: replace emoji favicon with png

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,10 +18,7 @@ export default function Layout({
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐣</text></svg>"
-        />
+        <link rel="icon" href="/hatching_chick.png" />
       </Head>
 
       <Header />
