6a8ff7a26bc404c8a8d14dbbdb7ab22272ad64c2
shakogegia <shalva.gegia@gmail.com>
2022-04-21 19:25:35 +0200

    fix: github pages nojekyll

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
   env: {
     ROOT: __dirname,
   },
-  assetPrefix: isProd ? '/shakogegia/' : '',
-  basePath: isProd ? '/shakogegia' : '',
 }
 
 module.exports = nextConfig
