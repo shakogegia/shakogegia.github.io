3b9a0619c5e19bd8489ffeabada317277692029f
shakogegia <shalva.gegia@gmail.com>
2022-04-22 16:10:03 +0200

    feat: inject google analytics

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/footer'
 import Header from '@/components/header'
 import Head from 'next/head'
+import Script from 'next/script'
 import { PropsWithChildren } from 'react'
 
 const me = 'Shalva Gegia'
@@ -23,6 +24,25 @@ export default function Layout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐣</text></svg>"
         />
       </Head>
+
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-110098374-1"
+      />
+
+      <Script
+        id="gtm"
+        dangerouslySetInnerHTML={{
+          __html: `
+              <!-- Global site tag (gtag.js) - Google Analytics -->
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-110098374-1');
+            `,
+        }}
+      />
 
       <Header />
 
