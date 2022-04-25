da18457774bf965f70dae8aa096d2ada604c2656
shakogegia <shalva.gegia@gmail.com>
2022-04-25 14:00:57 +0200

    Updates

---
+++ _next/static/lixbDq4aNc2m0zZOaRkxk/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Flibrary","\u002Fgarden","\u002Fabout","\u002Fblog","\u002Fnotes","\u002Fdrafts","\u002F","\u002Fdrafts\u002F[note]","\u002Fnotes\u002F[note]","\u002Farticle\u002F[article]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
