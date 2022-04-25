9427195f1752aaad42625081c7ae929c1e7ff377
shakogegia <shalva.gegia@gmail.com>
2022-04-25 12:46:33 +0200

    feat: support content directories

--- src/utils/fetch-article.ts
+++ src/utils/fetch-article.ts
@@ -1,5 +1,6 @@
-import { DATA_DIR } from '@/utils/config'
-import fs from 'fs/promises'
+import { DATA_DIR, PUBLIC_DIR } from '@/utils/config'
+import fs from 'fs'
+import glob from 'fast-glob'
 import path from 'path'
 import parseMarkdownArticle from 'src/utils/parse-markdown-article'
 
@@ -9,7 +10,41 @@ export default async function fetchArticle(
 ) {
   const dir = path.join(DATA_DIR, `${type}s`)
 
-  const markdown = await fs.readFile(path.join(dir, `${file}.md`), 'utf-8')
+  let markdown: string = ''
+
+  if (fs.existsSync(path.join(dir, `${file}.md`))) {
+    markdown = await fs.readFileSync(path.join(dir, `${file}.md`), 'utf-8')
+  } else if (fs.existsSync(path.join(dir, `${file}/index.md`))) {
+    markdown = await fs.readFileSync(
+      path.join(dir, `${file}/index.md`),
+      'utf-8'
+    )
+
+    const files = await glob(path.join(dir, `${file}/*.{png,jpg,gif}`))
+
+    const destDir = path.join(PUBLIC_DIR, `contents/${file}/`)
+
+    if (fs.existsSync(destDir)) {
+      fs.rmdirSync(destDir, { recursive: true })
+    } else {
+      fs.mkdirSync(destDir, { recursive: true })
+    }
+
+    files.forEach((item) => {
+      const fileName = path.basename(item)
+
+      fs.cpSync(item, path.join(destDir, `${fileName}`))
+
+      markdown = markdown.replaceAll(
+        `{ ${fileName} }`,
+        `/contents/${file}/${fileName}`
+      )
+    })
+  }
+
+  if (!markdown) {
+    throw new Error(`Article ${file} not found`)
+  }
 
   return parseMarkdownArticle(markdown)
 }
