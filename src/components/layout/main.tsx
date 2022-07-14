fe5ddbed4eb47472f35ee95bc4e0ba3906e1f702
shakogegia <shalva.gegia@gmail.com>
2022-07-14 11:34:33 +0200

    feat: gallery component

--- src/components/layout/main.tsx
+++ src/components/layout/main.tsx
@@ -2,17 +2,16 @@ import React, { PropsWithChildren, useEffect } from 'react'
 import classNames from '../../utils/classnames'
 import Footer from '../footer'
 import Header from '../header'
-import { Helmet } from "react-helmet"
+import { Helmet } from 'react-helmet'
 import Lightense from 'lightense-images'
 
 export default function Layout({ children, className }: PropsWithChildren<{ className?: string }>) {
-
   useEffect(() => {
-    Lightense('img');
+    Lightense('img', { keyboard: true })
   }, [])
 
   return (
-    <div className='h-vh min-h-full transition-colors dark:bg-gray-900'>
+    <div className="h-vh min-h-full transition-colors dark:bg-gray-900">
       <Helmet>
         <meta charSet="UTF-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
@@ -20,16 +19,9 @@ export default function Layout({ children, className }: PropsWithChildren<{ clas
 
         <meta name="theme-color" content="#6366f1" />
 
-        <script
-          type="module"
-          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-        />
-        <script
-          noModule
-          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-        />
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" />
+        <script noModule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js" />
       </Helmet>
-
 
       <Header />
       <main className={classNames('container mx-auto max-w-5xl px-6', className)}>{children}</main>
