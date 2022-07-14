dc5a54368ed9281b5826090923ea34617e0d1cc8
shakogegia <shalva.gegia@gmail.com>
2022-07-14 13:17:34 +0200

    fix: lazy load gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -5,7 +5,7 @@ export default function Gallery(props: PropsWithChildren<{ images: string[] }>) 
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 justify-items-center">
         {props.images.map((src) => (
-          <img key={src} className="rounded-md w-full h-full object-cover" src={src} />
+          <img loading="lazy" key={src} className="rounded-md w-full h-full object-cover" src={src} />
         ))}
       </div>
     </div>
