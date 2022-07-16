259ddb90dfced4c0ec78480cd36d04f15171d4e8
shakogegia <shalva.gegia@gmail.com>
2022-07-16 15:57:24 +0200

    fix: subtitle

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -17,14 +17,14 @@ export default function Post({ data: { site, mdx }, pageContext: { next, prev } 
       />
 
       <div className="max-w-3xl m-auto mt-8">
-        <h3 className="text-3xl max-w-lg mx-auto text-center iAWriterDuospaceBold transition-colors dark:text-gray-200">{mdx.frontmatter.title}</h3>
+        <h3 className="text-3xl max-w-lg mx-auto text-center transition-colors dark:text-gray-200">{mdx.frontmatter.title}</h3>
 
         <div className="prose max-w-none mt-16 prose-lg iAWriterDuospace break-words  text-gray-500 transition-colors dark:text-gray-300 dark:prose-a:text-gray-200 dark:prose-strong:text-gray-200 dark:prose-blockquote:text-gray-200">
           <MDXRenderer>{mdx.body}</MDXRenderer>
         </div>
 
         <div className="mt-14">
-          <em className="font-extralight iAWriterDuospaceItalic text-gray-400">Published on {mdx.frontmatter.date}</em>
+          <em className="font-extralight font-mono text-gray-400">Published on {mdx.frontmatter.date}</em>
         </div>
 
         <div className="py-20">
