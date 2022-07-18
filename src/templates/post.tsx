65ded309b5f9806693b40286665c91eadafb4a6a
shakogegia <shalva.gegia@gmail.com>
2022-07-18 18:51:06 +0200

    feat: cover

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,7 +1,6 @@
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import * as React from 'react'
-import Share from 'src/components/share'
 import Layout from '../components/layout/main'
 import SEO from '../components/seo'
 
@@ -17,23 +16,19 @@ export default function Post({ data: { site, mdx }, pageContext: { next, prev } 
       />
 
       <div className="max-w-3xl m-auto mt-8">
-        <h3 className="text-3xl max-w-lg mx-auto text-center transition-colors dark:text-gray-200">{mdx.frontmatter.title}</h3>
+        <h3 className="text-2xl iAWriterDuospaceBold transition-colors dark:text-gray-200">{mdx.frontmatter.title}</h3>
 
-        <div className="prose max-w-none mt-16 prose-lg iAWriterDuospace break-words  text-gray-500 transition-colors dark:text-gray-300 dark:prose-a:text-gray-200 dark:prose-strong:text-gray-200 dark:prose-blockquote:text-gray-200">
+        <div className="prose max-w-none mt-6 prose-lg iAWriterDuospace text-gray-500 transition-colors dark:text-gray-300 dark:prose-a:text-gray-200 dark:prose-strong:text-gray-200 dark:prose-blockquote:text-gray-200 dark:prose-headings:text-gray-200">
           <MDXRenderer>{mdx.body}</MDXRenderer>
         </div>
 
         <div className="mt-14">
-          <em className="font-extralight font-mono text-gray-400">Published on {mdx.frontmatter.date}</em>
+          <em className="font-extralight iAWriterDuospaceItalic text-gray-400">Published on {mdx.frontmatter.date}</em>
         </div>
 
         <div className="py-20">
           <div className="w-full border-t border-gray-100 transition-colors dark:border-gray-600"></div>
         </div>
-      </div>
-
-      <div className='mb-20'>
-        <Share url={`/${mdx.frontmatter.slug}`} text={mdx.frontmatter.title} />
       </div>
     </Layout>
   )
