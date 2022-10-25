8169688c3581ec9bbd809b43a72ddcd855a5de17
shakogegia <shalva.gegia@gmail.com>
2022-10-25 23:38:05 +0200

    Updates

---
+++ component---src-templates-redirect-tsx-04cd75807d711024bca0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[356],{2137:function(e,t,r){r.r(t),r.d(t,{default:function(){return i}});var n=r(7294),a=r(1082);function i(e){var t=e.pageContext;return(0,n.useEffect)((function(){var e=t.redirect.startsWith("/")?t.redirect:"/"+t.redirect;(0,a.navigate)(e)}),[t]),n.createElement("div",null)}}}]);
+//# sourceMappingURL=component---src-templates-redirect-tsx-04cd75807d711024bca0.js.map
