5c9ba4a151453d9a06631ad5f1b1aca0841a96ec
shakogegia <shalva.gegia@gmail.com>
2022-12-15 14:55:12 +0100

    docs: shooting 500T

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,14 +1,23 @@
 import React, { PropsWithChildren } from 'react'
 import classNames from 'src/utils/classnames'
 
-export default function Gallery(props: PropsWithChildren<{ images?: string[], className?: string }>) {
+export default function Gallery(props: PropsWithChildren<{ images?: string[]; className?: string }>) {
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 justify-items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-items-center">
         {props.children
           ? props.children
           : (props.images || []).map((src) => (
-              <img loading="lazy" key={src} className={classNames("rounded-md w-full h-full object-cover", props.className)} src={src} />
+              <img
+                loading="lazy"
+                key={src}
+                className={classNames(
+                  'rounded-lg w-full h-full object-cover',
+                  "w-64 h-64 m-0",
+                  props.className
+                )}
+                src={src}
+              />
             ))}
       </div>
     </div>
